8a2dfc5ec37d4c8cf28aed8c671bbea667173f58
Andrew <paladzin@gmail.com>
2019-04-15 18:22:19 +0300

    Add files via upload

---
+++ nginx/delayer_settings.py
@@ -0,0 +1,5 @@
+STREAM_DIR = '/tmp/nginx'
+STREAM_DESTINATION = 'rtmp://127.0.0.1:1935/live'
+DELAY = 60     #Seconds
+BACKUPSTREAM_SHORT = '' #Show while intermission
+BACKUPSTREAM_LONG = ''  #Show while longer downtime
