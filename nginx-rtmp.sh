2230196d78504e89298e9fc967b6a62ee9c32672
Andrew <paladzin@gmail.com>
2019-04-15 18:20:30 +0300

    Update nginx-rtmp.sh

--- nginx-rtmp.sh
+++ nginx-rtmp.sh
@@ -1,6 +1,6 @@
 docker run --detach --restart always -ti \
         --publish 1935:1935 \
-        --publish 80:81 \
+        --publish 80:80 \
         --volume /opt/nginx:/etc/nginx \
-        --name nginx-rtmp \
-        paladzin/nginx-rtmp
+        --name nginx-rtmp2d \
+        nginx-rtmp2d
